49653b06c25a82a40d3d3bf0a476254eb29ecd4b
wenyongqd <wenyongqd@gmail.com>
2020-01-18 09:14:51 -0500

    update ArrayQueue

--- src/Queue/ArrayQueueDemo.java
+++ src/Queue/ArrayQueueDemo.java
@@ -1,12 +1,61 @@
 package Queue;
 
-public class ArrayQueueDemo {
+import java.util.Scanner;
 
+public class ArrayQueueDemo {
     public static void main(String[] args) {
+        //创建一个队列
+        ArrayQueue arrayQueue = new ArrayQueue(3);
+        char key = ' '; //接受用户输入
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        //输出一个菜单
+        while (loop) {
+            System.out.println("s(show): 显示队列");
+            System.out.println("e(exit): 退出程序");
+            System.out.println("a(add): 添加数据到队列");
+            System.out.println("g(get): 从队列取出数据");
+            System.out.println("h(head): 查看队列头部");
+            key = scanner.next().charAt(0); //接收一个字符
+
+            switch (key) {
+                case 's':
+                    arrayQueue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输出一个数");
+                    int value = scanner.nextInt();
+                    arrayQueue.addQueue(value);
+                    break;
+                case 'g': //取出数据
+                    try {
+                        int res = arrayQueue.getQueue();
+                        System.out.printf("取出的数据是%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'h':
+                    try {
+                        int res = arrayQueue.headQueue();
+                        System.out.printf("队列头的数据是%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        System.out.println("程序退出");
 
     }
-
-
 }
 
 class ArrayQueue {
@@ -36,7 +85,7 @@ class ArrayQueue {
     //添加数据到队列
     public void addQueue(int n) {
         //判断队列是否满
-        if(isFull()) {
+        if (isFull()) {
             System.out.println("队列满，不能加入数据");
         }
         rear++;
@@ -46,7 +95,7 @@ class ArrayQueue {
     //获取队列的数据，出队列
     public int getQueue() {
         //判断队列是否为空
-        if(isEmpty()) {
+        if (isEmpty()) {
             //抛出异常
             throw new RuntimeException("队列为空，不能取数据");
         }
@@ -57,5 +106,21 @@ class ArrayQueue {
     //显示队列的所有数据
     public void showQueue() {
         //遍历
+        if (isEmpty()) {
+            System.out.println("队列空的，没有数据");
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("arr[%d]=%d\n", i, arr[i]);
+        }
+    }
+
+    //显示队列的头部，注意不是取出数据
+    public int headQueue() {
+        //判断
+        if (isEmpty()) {
+            throw new RuntimeException("队列空的，没有数据");
+        }
+        return arr[front + 1];
     }
 }
