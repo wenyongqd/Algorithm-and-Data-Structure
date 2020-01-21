884c811afc4489322531d3318947f589e4e30330
wenyongqd <wenyongqd@gmail.com>
2020-01-21 13:27:21 -0500

    updte Josepfu

--- src/LinkedList/Josepfu.java
+++ src/LinkedList/Josepfu.java
@@ -7,6 +7,11 @@ public class Josepfu {
         circleSingleLinkedList.addBoy(5); //加入5个小孩节点
         circleSingleLinkedList.showBoy();
 
+
+        //测试小孩出圈
+        circleSingleLinkedList.countBoy(1,2,5); //2->4->1->5->3
+
+
     }
 }
 
@@ -56,6 +61,55 @@ class CircleSingleLinkedList {
             }
             curBoy = curBoy.getNext(); //curBoy后移
         }
+    }
+
+    //根据用户的输入，计算出小孩出圈的顺序
+
+    /**
+     *
+     * @param startNo 表示从第几个小孩开始数数
+     * @param countNo 表示数几下
+     * @param nums 表示最初有多少个小孩在圈中
+     */
+    public void countBoy(int startNo, int countNo, int nums) {
+        //先对数据进行校验
+        if (first == null  || startNo < 1 || startNo > nums ) {
+            System.out.println("参数输入有误，请重新输入");
+            return;
+        }
+        //创建辅助指针，帮助小孩出圈
+        Boy helper = first;
+        //需求创建一个辅助指针（变量）helper，事先应该指向环形量表的最后这个节点
+        while (true) {
+            if (helper.getNext() == first) {  //说明helper指向最后小孩节点
+                break;
+            }
+            helper = helper.getNext();
+        }
+        //小孩报数前，先让first和helper移动k - 1次
+        for (int j = 0; j < startNo - 1; j++) {
+            first = first.getNext();
+            helper = helper.getNext();
+        }
+        //当小孩报数时，让first和helper指针同时移动m-1次，然后出圈
+        //这里是一个循环操作，直到圈中只有一个节点
+        while (true) {
+            if (helper == first) { //说明圈中只有一个节点
+                break;
+            }
+            //让first个helper指针同时移动countNum-1次
+            for (int j =0; j < countNo - 1; j++) {
+                first = first.getNext();
+                helper = helper.getNext();
+            }
+            //这时first指向的节点，就是要出圈的小孩节点
+            System.out.printf("小孩%d出圈\n", first.getNo());
+            //这时将first指向的小孩节点出圈
+            first = first.getNext();
+            helper.setNext(first);
+        }
+        System.out.printf("最后留在圈中的小孩编号是%d \n", first.getNo());
+
     }
 }
 
