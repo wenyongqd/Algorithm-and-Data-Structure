837a7da12e41b0b09353525c34aaeae077f5d29a
wenyongqd <wenyongqd@gmail.com>
2020-01-22 22:26:46 -0500

    update stack

--- src/Stack/Calculator.java
+++ src/Stack/Calculator.java
@@ -3,7 +3,8 @@ package Stack;
 public class Calculator {
     public static void main(String[] args) {
         //根据前面老师的思路，完成表达式的计算
-        String expression = "3+2*6-2";
+//        String expression = "3+2*6-2";
+        String expression = "70+2*6-2";
         //创建两个栈，数栈和符号栈
         ArrayStack2 numStack = new ArrayStack2(10);
         ArrayStack2 operStack = new ArrayStack2(10);
@@ -14,14 +15,15 @@ public class Calculator {
         int oper = 0;
         int res = 0;
         char ch = ' '; //将每次扫描的char保存到ch
+        String keepNum = ""; //用于拼接多位数
         //开始while语句循环的扫面
-        while(true) {
+        while (true) {
             //依次得到expression的每一个字符
             ch = expression.substring(index, index + 1).charAt(0);
             //判断ch是什么，然后做相应的处理
             if (operStack.isOper(ch)) { //如果是运算符
                 //判断当前的符号栈是否为空
-                if(!operStack.isEmpty()) {
+                if (!operStack.isEmpty()) {
                     //处理
                     //如果符号栈有操作符，就进行比较，如果当前的操作符的优先级小于或等于栈中的操作符，就需要从数栈中pop出两个数
                     //在从符号栈中pop出一个符号，进行运算，将得到结果，入数栈，然后将当前的操作符入符号栈
@@ -38,13 +40,33 @@ public class Calculator {
                         //如果当前的操作符的优先级大于栈中的操作符，就直接入符号栈
                         operStack.push(ch);
                     }
-                }else {
+                } else {
                     //如果为空，直接入符号栈
                     operStack.push(ch);
-
                 }
             } else {  //如果是数，则直接入数栈
-                numStack.push(ch - 48);
+//                numStack.push(ch - 48);
+
+                //1.当处理多位数时，不能发现是一个数就立即入栈，因为它可能是多位数
+                //2.在处理数，需要向expression的表达式的index后再看一位，如果是数就进行扫描，如果是符号才入栈
+                //3.因此我们需要定义一个变量，字符串，用于拼接
+
+                keepNum += ch;
+
+                //如果ch已经是最后一位，就直接入栈
+                if (index == expression.length() - 1) {
+                    numStack.push(Integer.parseInt(keepNum));
+                } else {
+                    //判断下一个字符是不是数字，如果是数字就继续扫描，如果是运算符，则入栈
+                    //只是往后看一位，不是index++
+                    if (operStack.isOper(expression.substring(index + 1, index + 2).charAt(0))) {
+                        //如果后一位是运算符，则入栈keepNum="1"或"123"
+                        numStack.push(Integer.parseInt(keepNum));
+                        //重要！！！keepNum要清空
+                        keepNum = "";
+                    }
+                }
+
             }
 
             //让index + 1，并判断是否扫描到expression最后
@@ -131,12 +153,13 @@ class ArrayStack2 {
         }
         //需要从栈顶开始显示数据
         for (int i = top; i >= 0; i--) {
-            System.out.printf("stack[%d]=%d\n", i,stack[i]);
+            System.out.printf("stack[%d]=%d\n", i, stack[i]);
         }
     }
+
     //返回运算符的优先级，优先级是程序员定的，优先级使用数字表示
     //数字越大，优先级则越高
-     public int priority(int oper) {
+    public int priority(int oper) {
         if (oper == '*' || oper == '/') {
             return 1;
         } else if (oper == '+' || oper == '-') {
@@ -144,15 +167,15 @@ class ArrayStack2 {
         } else {
             return -1; //假定目前的表达式只有+，——，*，/
         }
-     }
+    }
 
-     //判断是不是一个运算符
+    //判断是不是一个运算符
     public boolean isOper(char val) {
-        return val == '+' || val == '-' || val == '*' || val == '/';9
+        return val == '+' || val == '-' || val == '*' || val == '/';
     }
 
     //计算方法
-    public int cal (int num1, int num2, int oper) {
+    public int cal(int num1, int num2, int oper) {
         int res = 0; //用于存放计算的结果
         switch (oper) {
             case '+':
@@ -162,10 +185,10 @@ class ArrayStack2 {
                 res = num2 - num1;
                 break;
             case '*':
-                res = num1*num2;
+                res = num1 * num2;
                 break;
             case '/':
-                res = num2/num1;
+                res = num2 / num1;
                 break;
         }
         return res;
