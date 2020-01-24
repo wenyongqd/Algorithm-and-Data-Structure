fcfc00a3d4220f1631d57fc7ee4f8c1ebb8f037a
wenyongqd <wenyongqd@gmail.com>
2020-01-23 21:52:33 -0500

    update PolandNotion

--- src/Stack/PolandNotion.java
+++ src/Stack/PolandNotion.java
@@ -2,6 +2,7 @@ package Stack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Stack;
 
 public class PolandNotion {
@@ -10,12 +11,17 @@ public class PolandNotion {
         //说明
         //1. 1+((2+3)*4)-5 => 转成 1 2 3 + 4 * + 5 -
         //2. 因为直接对str进行操作，不方便，因此先将"1+((2+3)*4)-5" => 中缀表达式对应的list
-        //  即："1+((2+3)*4)-5" => ArrayList[1,+,(,(,2,+,3,),*,4,),-,5]
+        //   即："1+((2+3)*4)-5" => ArrayList[1,+,(,(,2,+,3,),*,4,),-,5]
+        //3. 将得到的中缀表达式对应的List => 后缀表达式对应的List
+        //   即ArrayList[1,+,(,(,2,+,3,),*,4,),-,5] => ArrayList[1,2,3,+,4,*,+,5,-]
         String expression = "1+((2+3)*4)-5";
 
         //测试
         List<String> infixExpressionList = toInfixExpressionList(expression);
-        System.out.println(infixExpressionList);
+        System.out.println("中缀表达式对应的List=" + infixExpressionList);
+
+        List<String> ufficExpressionList = parseSuffixExpressionList(infixExpressionList);
+        System.out.println("后缀表达式对应的List=" + ufficExpressionList);
 
         //先定义一个逆波兰表达式
         //(3+4)*5-6 => 3 4 + 5 * 6 -
@@ -32,6 +38,47 @@ public class PolandNotion {
 
     }
 
+    public static List<String> parseSuffixExpressionList(List<String> ls) {
+        //定义两个栈
+        Stack<String> s1 = new Stack<>(); //符号栈
+        //说明，因为s2这个栈，在整个转换过程中，没有pop操作，而且后面我们还需要逆序输出
+        //依次比较麻烦，这里我么不用Stack<String>, 直接使用List<String> s2
+        List<String> s2 = new ArrayList<>();
+
+        //遍历ls
+        for (String item : ls) {
+            //如果是一个数，加入s2
+            if (item.matches("\\d+")) {
+                s2.add(item);
+            } else if (item.equals("(")) {
+                s1.push(item);
+            } else if (item.equals("")) {
+                //如果是右括号"）"，则依次弹出s1栈的运算符，并压入s2，直到遇左括号为止，此时将这一堆括号丢弃
+                while (!s1.peek().equals("(")) {
+                    s2.add(s1.pop());
+                }
+                s1.pop();
+            } else {
+                //当item的优先级小于等于s1栈顶运算符，将s1栈顶的运算符弹出并加入到s2中，再次转到(4.1)与
+                //s1中新的栈顶运算符相比较
+                //问题：缺少一个比较优先级高低的方法
+                while (s1.size() != 0 && Operation.getValue(s1.peek()) != Operation.getValue(item)) {
+                    s2.add(s1.pop());
+                }
+                //还需要将item压入栈
+                s1.push(item);
+            }
+        }
+
+        //将s1中剩余的运算符依次弹出并加入s2
+        while (s1.size() != 0) {
+            s2.add(s1.pop());
+        }
+
+        return s2; //因为是存放到List，因此按顺序输出就是对应的后缀表达式对应的List
+
+    }
+
     //方法，将中缀表达式转成对应的List
     public static List<String> toInfixExpressionList(String s) {
         //定义一个List，存放中缀表达式对应的内容
@@ -41,12 +88,12 @@ public class PolandNotion {
         char c; //每遍历到一个字符，就放入到c
         do {
             //如果c是一个非数字，我需要加入到ls
-            if ((c=s.charAt(i)) < 48 || (c=s.charAt(i)) > 57) {
+            if ((c = s.charAt(i)) < 48 || (c = s.charAt(i)) > 57) {
                 ls.add("" + c);
                 i++;  //i需要后移
             } else { //如果是一个数，需要考虑多位数的问题
                 str = ""; //先将str置成""空串
-                while (i < s.length() && (c=s.charAt(i)) >= 48 && (c = s.charAt(i)) <= 57) {
+                while (i < s.length() && (c = s.charAt(i)) >= 48 && (c = s.charAt(i)) <= 57) {
                     str += c; //拼接
                     i++;
                 }
@@ -97,7 +144,7 @@ public class PolandNotion {
                 } else if (item.equals("-")) {
                     res = num1 - num2;
                 } else if (item.equals("*")) {
-                    res = num1*num2;
+                    res = num1 * num2;
                 } else if (item.equals("/")) {
                     res = num1 / num2;
                 } else {
@@ -110,5 +157,36 @@ public class PolandNotion {
         }
         //最后留在stack中的数据就是运算结果
         return Integer.parseInt(stack.pop());
+    }
+}
+
+//编写一个类Operation，可以返回一个运算符对应的优先级
+class Operation {
+    private static int ADD = 1;
+    private static int SUB = 1;
+    private static int MUL = 2;
+    private static int DIV = 2;
+
+    //写一个方法，返回对应的优先级数字
+    public static int getValue(String operation) {
+        int result = 0;
+        switch (operation) {
+            case "+":
+                result = ADD;
+                break;
+            case "-":
+                result = SUB;
+                break;
+            case "*":
+                result = MUL;
+                break;
+            case "/":
+                result = DIV;
+                break;
+            default:
+                System.out.println("不存在该运算符");
+                break;
+        }
+        return result;
     }
 }
