e1bae8393a1d7736819d690d87f1cff2d087f750
wenyongqd <wenyongqd@gmail.com>
2020-03-01 23:46:19 -0500

    update problem 102

---
+++ datestructure/leetCode/Tree/Solution_102.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// i: 表示第几个
+// j: 表示第几层
+class Solution_102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+ArrayList ans = new ArrayList();
+        ArrayList next = new ArrayList();
+        ArrayList curNode = new ArrayList();
+        curNode.add(root);
+           
+        while (!curNode.isEmpty() ) {
+            ArrayList temp = new ArrayList();
+            for (Object curNodes : curNode) {
+                if ( curNodes != null) {
+                    temp.add(((TreeNode) curNodes).val);
+                    next.add(((TreeNode) curNodes).left);
+                    next.add(((TreeNode) curNodes).right);
+                }
+            }
+            if(!temp.isEmpty()) {
+                ans.add(temp);
+            }
+            curNode = (ArrayList) next.clone();
+            next.clear(); 
+        }
+        return ans;
+    }
+}
--- datestructure/leetCode/Tree/Solution_96.java
+++ datestructure/leetCode/Tree/Solution_96.java
@@ -1,7 +1,6 @@
 class Solution_96 {
     public int numTrees(int n) {
         int[] G = new int[n+1];
-        int res = 0;
         
         G[0] = 1;
         G[1] = 1;
@@ -9,9 +8,10 @@ class Solution_96 {
         for (int j = 2; j <=n; j++) {
             
             for(int i = 1; i <= j; i++ ) {
-                G[j] = G[i-1]*G[j-i]  + G[j];                
+                G[j] = G[i-1]*G[j-i] + G[j];    //这里使用了递归            
             }
         }
         return G[n];
     }
 }
+
