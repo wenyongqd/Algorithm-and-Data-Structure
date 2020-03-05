67ff9349c0b74f1703b3960c8a1b05b4bf73da0b
wenyongqd <wenyongqd@gmail.com>
2020-03-05 18:55:53 -0500

    update problem 508

---
+++ datestructure/leetCode/Tree/Solution_508.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+    int sum = 0;
+    int max = 0;
+    int temp;
+    public int[] findFrequentTreeSum(TreeNode root) {
+        map = helper2(root,map,temp);      
+        for (Integer value : map.values()) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        
+        List<Integer> list = new ArrayList<Integer>();
+        for(int key : map.keySet()){
+            if(map.get(key) == max){
+                list.add(key);
+            }
+        }
+        int[] result = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            result[i] = list.get(i);
+        }
+        if (root == null) return new int[]{};
+        if (map.get(list.get(list.size()-1)) == max) {
+            return result;
+        } else {
+            return new int[]{result[list.size()-1]};
+        }  
+              
+    }
+
+    
+    public int helper(TreeNode root) {
+        if (root == null) return 0;
+        sum = root.val + sum;
+        helper(root.left);
+        helper(root.right);
+
+        return sum;
+    }
+    public Map<Integer,Integer> helper2(TreeNode root, Map<Integer, Integer> map, int temp) {
+        if (root == null) return map;
+        temp = 0;
+        sum = 0;
+        temp = helper(root);
+        if (map.containsKey(temp)) {
+            map.put(temp, map.get(temp) + 1);
+        } else {
+           map.put(temp, 0); 
+        }
+        
+        helper2(root.left, map, temp);
+        helper2(root.right, map,temp);
+        
+        return map;
+    }
+}
